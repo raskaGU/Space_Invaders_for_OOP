aa01e008b21e5e871047a6ddc1b4fa4e1ba9018f
TatianaMaksakova <tatzianar@gmail.com>
2023-10-12 18:34:04 +0300

    add: add class Character update: update start_game function

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import pygame
 import sys
 
+def start_game():
+    pygame.init()
+    screen = pygame.display.set_mode((500,900))
+    pygame.display.set_caption("SpaceX by Gulamova")
+
+
+    flag = True
+    while flag:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        pygame.display.flip()
+
+
+start_game()
